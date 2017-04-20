3302305bd839a2a51bd4b858501e8392b4615dfb
Thomas Saunders <saun0063@umn.edu>
2017-04-20 11:43:58 -0500

    move ruport class eval fun to an initializer

--- lib/spree_advanced_reporting.rb
+++ lib/spree_advanced_reporting.rb
@@ -1,5 +1,4 @@
 require 'advanced_reporting_hooks'
-require 'ruport'
 
 module Spree
   module AdvancedReporting
@@ -22,31 +21,6 @@ module Spree
         #   Mime::Type.register('application/pdf', :pdf)
         # end
 
-        Ruport::Formatter::HTML.class_eval do
-          # Renders individual rows for the table.
-          def build_row(data = self.data)
-            @odd = !@odd
-            klass = @odd ? "odd" : "even"
-            output <<
-            "\t\t<tr class=\"#{klass}\">\n\t\t\t<td>" +
-              data.to_a.join("</td>\n\t\t\t<td>") +
-              "</td>\n\t\t</tr>\n"
-          end
-
-          def html_table
-            @odd = false
-            "<table class=\"tablesorter\">\n" << yield << "</table>\n"
-          end
-
-          def build_table_header
-            output << "\t<table class=\"tablesorter\">\n"
-            unless data.column_names.empty? || !options.show_table_headers
-              output << "\t\t<thead><tr>\n\t\t\t<th>" +
-                data.column_names.join("</th>\n\t\t\t<th>") +
-                "</th>\n\t\t</tr></thead>\n"
-            end
-          end
-        end
       end
 
       config.to_prepare &method(:activate).to_proc
