4b7a4936faf5640e2fd3e1422bb564c92fd33d9c
Austin Grandt <austin.grandt@gmail.com>
2020-02-07 14:51:43 -0600

    first pass at expanding the order details report

--- app/models/spree/advanced_report/order_detail_report.rb
+++ app/models/spree/advanced_report/order_detail_report.rb
@@ -21,19 +21,24 @@ class Spree::AdvancedReport::OrderDetailReport < Spree::AdvancedReport
                        else
                          'pending'
                        end
+        
+      subscription = if order.account_subscriptions.any?
+                        # Assume only one subscription for now
+                        order.account_subscriptions.first
+                      end
 
       order.line_items.each do |li|
-        lines << line_item_report_line(li, order, transaction_id)
+        lines << line_item_report_line(li, order, transaction_id, subscription)
       end
 
       order.all_adjustments.eligible.each do |adj|
-        lines << adjustment_report_line(adj, order, transaction_id)
+        lines << adjustment_report_line(adj, order, transaction_id, subscription)
       end
 
       next unless order.ship_total?
 
       order.shipments.where('cost > 0').each do |shipment|
-        lines << shipment_report_line(shipment, order, transaction_id)
+        lines << shipment_report_line(shipment, order, transaction_id, subscription)
       end
     end
 
@@ -59,48 +64,68 @@ class Spree::AdvancedReport::OrderDetailReport < Spree::AdvancedReport
   ReportLine = Struct.new(
     :number,
     :completed_date,
+    :company,
+    :billing_address,
     :sku,
     :quantity,
     :unit_price,
     :amount,
-    :transaction_id
+    :transaction_id,
+    :subscription_id,
+    :subscription_token,
+    :subscription_expiration,
   )
 
   private
 
-  def line_item_report_line(line_item, order, transaction_id)
+  def line_item_report_line(line_item, order, transaction_id, subscription)
     ReportLine.new(
       order.number,
       order.completed_at.strftime('%m/%d/%Y'),
+      order.billing_address.company,
+      "%s %s %s, %s %s, %s" % [order.billing_address.address1, order.billing_address.address2, order.billing_address.city, order.billing_address.state_text, order.billing_address.zipcode, order.billing_address.country.to_s],
       line_item.variant.sku,
       line_item.quantity,
       line_item.price.to_f,
       (line_item.price * line_item.quantity).to_f,
-      transaction_id
+      transaction_id,
+      subscription.presence ? subscription.id : '',
+      subscription.presence ? subscription.token : '',
+      subscription.presence ? subscription.end_datetime.strftime('%m/%d/%Y') : '',
     )
   end
 
-  def adjustment_report_line(adj, order, transaction_id)
+  def adjustment_report_line(adj, order, transaction_id, subscription)
     ReportLine.new(
       order.number,
       order.completed_at.strftime('%m/%d/%Y'),
+      order.billing_address.company,
+      "%s %s %s, %s %s, %s" % [order.billing_address.address1, order.billing_address.address2, order.billing_address.city, order.billing_address.state_text, order.billing_address.zipcode, order.billing_address.country.to_s],
       adj.label,
       nil,
       nil,
       adj.amount.to_f,
-      transaction_id
+      transaction_id,
+      subscription.presence ? subscription.id : '',
+      subscription.presence ? subscription.token : '',
+      subscription.presence ? subscription.end_datetime.strftime('%m/%d/%Y') : '',
     )
   end
 
-  def shipment_report_line(shipment, order, transaction_id)
+  def shipment_report_line(shipment, order, transaction_id, subscription)
     ReportLine.new(
       order.number,
       order.completed_at.strftime('%m/%d/%Y'),
+      order.billing_address.company,
+      "%s %s %s, %s %s, %s" % [order.billing_address.address1, order.billing_address.address2, order.billing_address.city, order.billing_address.state_text, order.billing_address.zipcode, order.billing_address.country.to_s],
       shipment.shipping_method.try(:name) || 'Unknown shipping method',
       nil,
       nil,
       shipment.cost.to_f,
-      transaction_id
+      transaction_id,
+      subscription.presence ? subscription.id : '',
+      subscription.presence ? subscription.token : '',
+      subscription.presence ? subscription.end_datetime.strftime('%m/%d/%Y') : '',
     )
   end
 end
